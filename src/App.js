9247115259fc1dd801145478470a614f1e7eb82b
Nagateja <valivetiteja2004@gmail.com>
2022-08-09 14:35:41 +0530

    Project Done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React , {useState} from 'react';
+import React , {useState,useEffect} from 'react';
 import Values from 'values.js';
 import Color from './Color';
 
@@ -10,22 +10,33 @@ function App() {
   function submit(e) {
     e.preventDefault()
     try {
+      setError(false)
       let allColors = new Values(color).all(10);
       setGotColors(allColors)
       
     } catch (error) {
-      setError(true)
+      setError(true);
+      setGotColors([]);
       console.error("Error : " + error)
     }
   }
+
+  useEffect(() => {
+    let clr = setTimeout(()=>{
+      setError(false)
+    },2000)
+    return ()=>clearTimeout(clr)
+  }, [error])
+
   return (
     <>
       <section className="form">
         <h1>HexaColors</h1>
         <form onSubmit={submit}>
-          <input type="text" id="color" className={`${error ? `errorColor` :  `inputColor`}`} placeholder="#ffffff" value={color} onChange={(e)=>{setColor(e.target.value)}}/>
+          <input type="text" id="color" className={`inputColor`} placeholder="#ffffff" value={color} onChange={(e)=>{setColor(e.target.value)}}/>
           <button type="submit">Colors</button>
         </form>
+        {error && <h1 style={{fontFamily:'cursive',color:'#000000',fontSize:'7vmin'}}>No Color Found</h1>}
       </section>
       <section className="colors">
         {gotColors.map((eachColor,index)=>{
